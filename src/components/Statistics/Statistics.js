8ddf97e56f18941f40601e14e01759e678b8fa52
cbHasib <hasibul.pbn@gmail.com>
2022-10-11 02:20:46 +0600

    Added Statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const data = useLoaderData();
+  console.log(data.data);
+
+  return (
+    <div className="max-w-xl mx-auto py-10">
+      <BarChart width={500} height={400} data={data.data}>
+        <Bar dataKey="total" fill="#570DF8" />
+        <XAxis dataKey="name" />
+        <YAxis/>
+        <Tooltip />
+        <Legend />
+      </BarChart>
+    </div>
+  );
 };
 
 export default Statistics;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog/Blog";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Home from "../components/Home/Home";
+import Statistics from "../components/Statistics/Statistics";
 import Main from "../layout/Main";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -21,6 +22,12 @@ export const routes = createBrowserRouter([
       {
         path: "/blog",
         element: <Blog></Blog>,
+      },
+      {
+        path: "/statistics",
+        loader: async () =>
+          fetch("https://openapi.programming-hero.com/api/quiz"),
+        element: <Statistics></Statistics>,
       },
     ],
   },
